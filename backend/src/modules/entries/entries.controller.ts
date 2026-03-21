5918f2e6af47f46f890044e9b1d75a18a2eb00d0
Oaken Travis <oakentravis@gmail.com>
2026-03-21 15:08:52 +0000

    add post method to controller

--- backend/src/modules/entries/entries.controller.ts
+++ backend/src/modules/entries/entries.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
+import { EntriesService } from './entries.service';
 
 @Controller('entries')
 export class EntriesController {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private service: EntriesService,
+  ) {}
 
   @Get()
   async getAll() {
-    return this.prisma.entry.findMany();
+    return this.service.findAll();
+  }
+  @Post()
+  create(@Body() body: { title: string; duration: number }) {
+    return this.service.create(body);
   }
 }
