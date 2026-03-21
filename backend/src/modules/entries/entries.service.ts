5447eb1984d220c97eff07d6479537d185bb5b19
Oaken Travis <oakentravis@gmail.com>
2026-03-21 14:58:36 +0000

    start to add api routes

--- backend/src/modules/entries/entries.service.ts
+++ backend/src/modules/entries/entries.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class EntriesService {}
+export class EntriesService {
+  constructor(private prisma: PrismaService) {}
+  findAll() {
+    return this.prisma.entry.findMany();
+  }
+  create(data: { title: string; duration: number }) {
+    return this.prisma.entry.create({
+      data,
+    });
+  }
+}
