03bfc193893dcd90f221cbabf300497d21607d21
Oakent <oakentravis@gmail.com>
2026-03-21 14:06:46 +0000

    chore: merge frontend/backend ignore rules into root .gitignore

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { EntriesModule } from './modules/entries/entries.module';
 
 async function bootstrap() {
+  console.log('database url:', process.env.DATABASE_URL);
   const app = await NestFactory.create(EntriesModule);
   app.enableCors({
     origin: 'localhost:5173',
