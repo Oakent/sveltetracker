7c41ee8dc52f2dc4df868f43b50b1375a8898c73
Oaken Travis <oakentravis@gmail.com>
2026-03-30 22:10:27 +0100

    fix emojis

--- src/lib/components/twemoji.svelte
+++ src/lib/components/twemoji.svelte
@@ -15,11 +15,23 @@
 		twemoji.parse(emoji, {
 			base: 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/',
 			ext: '.png',
-			size: `${size}x${size}`
+			size: '72x72'
 		})
 	);
 </script>
 
-<span class={className}>
+<span
+	class={className}
+	style="width: {size}px; height: {size}px; display: inline-flex; align-items: center; justify-content: center;"
+>
+	<!-- svelte-ignore a11y_interesting_non_elements_with_interactive_events_have_interactive_roles -->
 	{@html parsedHtml}
 </span>
+
+<style>
+	:global(span img.emoji) {
+		width: 100%;
+		height: 100%;
+		object-fit: contain;
+	}
+</style>
